74d13da8c76768780c80378c17688b4e7d119c6d
27marques <paty_marques27@hotmail.com>
2022-04-24 21:05:57 -0300

    Usuario, Categoria e Tags

--- Controllers/Usuario.cs
+++ Controllers/Usuario.cs
@@ -8,7 +8,6 @@ namespace Controllers
     public class UsuarioController 
     {
         public static Usuario InserirUsuario(
-            int Id,
             string Nome,
             string Email,
             string Senha
@@ -35,6 +34,61 @@ namespace Controllers
             }
 
             return new Usuario(Nome, Email, Senha);
+        }
+
+        public static Usuario AlterarUsuario(
+            int Id,
+            string Nome,
+            string Email,
+            string Senha
+        )
+        {
+            Usuario usuario = GetUsuario(Id);
+
+            if (!String.IsNullOrEmpty(Nome))
+            {
+                usuario.Nome = Nome;
+            }
+
+            if (!String.IsNullOrEmpty(Email))
+            {
+                usuario.Email = Email;
+            }
+
+            if (!String.IsNullOrEmpty(Senha))
+            {
+                usuario.Senha = BCrypt.Net.BCrypt.HashPassword(Senha);
+            }
+            return usuario;
+        }
+
+        public static Usuario ExcluirUsuario(
+            int Id
+        )
+        {
+            Usuario usuario = GetUsuario(Id);
+            Usuario.RemoverUsuario(usuario);
+            return usuario;
+        }
+        public static List<Usuario> VisualizarUsuario()
+        {
+            return Usuario.GetUsuarios();
+        }
+
+        public static Usuario GetUsuario(int Id)
+        {
+            Usuario usuario = (
+                from Dentista in Usuario.GetUsuarios()
+                    where Usuario.Id == Id
+                    select Usuario
+            ).First();
+
+            if (usuario == null)
+            {
+                throw new Exception("Usuário não encontrado");
+            }
+
+            return usuario;
         }
     }
 }
