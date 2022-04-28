9a92230d732172f8e3cf5baa2cd990326873accb
27marques <paty_marques27@hotmail.com>
2022-04-27 22:04:39 -0300

    View Login

--- Views/Lib/Pessoa.cs
+++ Views/Lib/Pessoa.cs
@@ -7,8 +7,6 @@ namespace Views.Lib
     public class Pessoa : BaseForm
     {
         FieldForm fieldNome;
-        FieldForm fieldCpf;
-        FieldForm fieldTelefone;
         FieldForm fieldEmail;
         FieldForm fieldSenha;
                      
@@ -16,17 +14,11 @@ namespace Views.Lib
         public Pessoa() : base("Pessoa",SizeScreen.Medium)
         {
             fieldNome = new FieldForm("Nome",20,20,400,20);
-            fieldCpf = new FieldForm("CPF",20,80,120,20);
-            fieldTelefone = new FieldForm("Telefone",20,140,120,20);
-            fieldEmail = new FieldForm("E-mail",20,200,400,20);
-            fieldSenha = new FieldForm("Senha",20,260,120,20);
+            fieldEmail = new FieldForm("E-mail",20,80,120,20);
+            fieldSenha = new FieldForm("Senha",20,140,120,20);
 
             this.Controls.Add(fieldNome.lblField);
             this.Controls.Add(fieldNome.txtField);
-            this.Controls.Add(fieldCpf.lblField);
-            this.Controls.Add(fieldCpf.txtField);
-            this.Controls.Add(fieldTelefone.lblField);
-            this.Controls.Add(fieldTelefone.txtField);
             this.Controls.Add(fieldEmail.lblField);
             this.Controls.Add(fieldEmail.txtField);
             this.Controls.Add(fieldSenha.lblField);
--- Views/Login.cs
+++ Views/Login.cs
@@ -12,21 +12,21 @@ namespace Views
 {
     public class Login : BaseForm
     {
-        FieldForm fieldUsuario;
+        FieldForm fieldNome;
         FieldForm fieldSenha;
 		ButtonForm btnConfirmar;
         ButtonForm btnCancelar;
 
         public Login() : base("Login",SizeScreen.Small)
         {
-            fieldUsuario = new FieldForm("Usuário",20,20,180,20);
+            fieldNome = new FieldForm("Usuário",20,20,180,20);
             fieldSenha = new FieldForm("Senha",20,100,180,60);
 
 			btnConfirmar = new ButtonForm("Confirmar", 50, 220, this.handleConfirm);
             btnCancelar = new ButtonForm("Cancelar", 180, 220, this.handleCancel);
 
-            this.Controls.Add(fieldUsuario.lblField);
-            this.Controls.Add(fieldUsuario.txtField);
+            this.Controls.Add(fieldNome.lblField);
+            this.Controls.Add(fieldNome.txtField);
             this.Controls.Add(fieldSenha.lblField);
             this.Controls.Add(fieldSenha.txtField);
             this.Controls.Add(btnConfirmar);
