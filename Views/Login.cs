d7a3cc0704193414c1e1998decd9a1acf5a8d905
27marques <paty_marques27@hotmail.com>
2022-05-15 10:09:25 -0300

    AjusteLoginUsuario

--- Views/Login.cs
+++ Views/Login.cs
@@ -7,26 +7,27 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Threading;
 using Views.Lib;
+using Controllers;
 
 namespace Views
 {
     public class Login : BaseForm
     {
-        FieldForm fieldNome;
+        FieldForm fieldUsuario;
         FieldForm fieldSenha;
 		ButtonForm btnConfirmar;
         ButtonForm btnCancelar;
 
         public Login() : base("Login",SizeScreen.Small)
         {
-            fieldNome = new FieldForm("Usuário",20,20,180,20);
+            fieldUsuario = new FieldForm("Usuário",20,20,180,20);
             fieldSenha = new FieldForm("Senha",20,100,180,60);
 
 			btnConfirmar = new ButtonForm("Confirmar", 50, 220, this.handleConfirm);
             btnCancelar = new ButtonForm("Cancelar", 180, 220, this.handleCancel);
 
-            this.Controls.Add(fieldNome.lblField);
-            this.Controls.Add(fieldNome.txtField);
+            this.Controls.Add(fieldUsuario.lblField);
+            this.Controls.Add(fieldUsuario.txtField);
             this.Controls.Add(fieldSenha.lblField);
             this.Controls.Add(fieldSenha.txtField);
             this.Controls.Add(btnConfirmar);
@@ -34,12 +35,21 @@ namespace Views
         }
 
         private void handleConfirm(object sender, EventArgs e)
+
         {
-            //verificar se pode logar
-            (new MenuPrincipal()).Show();
+            try {
+                UsuarioController.Auth(
+                    this.fieldUsuario.txtField.Text,
+                    this.fieldSenha.txtField.Text
+                );
+                (new MenuPrincipal()).Show();
+
+            } catch (Exception err) {
+                MessageBox.Show(err.Message);
+            }
 
         }
-
+       
         private void handleCancel(object sender, EventArgs e)
         {
             this.Close();
