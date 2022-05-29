8394a74fbbad5a1fdbdc4cc9e7e91b9623dd3456
27marques <paty_marques27@hotmail.com>
2022-05-29 11:23:05 -0300

    CrudCategoria

--- Views/CrudCategoria.cs
+++ Views/CrudCategoria.cs
@@ -7,38 +7,41 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Threading;
 using Views.Lib;
+using Models;
+using Controllers
 
 namespace Views
 {
     public class CrudCategoria : BaseForm
     {
+        Form parent;
         ListView listView;
         ButtonForm btnCadastrar;
         ButtonForm btnEditar;
         ButtonForm btnExcluir;
         ButtonForm btnCancelar;
        
-        public CrudCategoria() : base("Lista de Categorias",SizeScreen.Small)
+        public CrudCategoria(Form parent) : base("Lista de Categorias",SizeScreen.Small)
         {
+            this.parent = parent;
+            this.parent.Hide();
             listView = new ListView();
 			listView.Location = new Point(10, 20);
 			listView.Size = new Size(280,180);
             listView.View = View.Details;
-            ListViewItem Categoria1 = new ListViewItem("XXXXX");
-			Categoria1.SubItems.Add("ZZZZZZ");
-            listView.Items.AddRange(new ListViewItem[]{Categoria1});
-			listView.Columns.Add("Nome", -2, HorizontalAlignment.Left);
+            listView.Columns.Add("Nome", -2, HorizontalAlignment.Left);
     		listView.Columns.Add("Descrição", -2, HorizontalAlignment.Left);
 			listView.FullRowSelect = true;
 			listView.GridLines = true;
 			listView.AllowColumnReorder = true;
 			listView.Sorting = SortOrder.Ascending;
 			
-            btnCadastrar = new ButtonForm("Cadastrar", 10, 220, this.CadastrarCategoria);
-            btnEditar = new ButtonForm("Editar", 10, 260, this.EditarCategoria);
-            btnExcluir = new ButtonForm("Excluir", 120, 220, this.ExcluirCategoria);
+            btnCadastrar = new ButtonForm("Cadastrar", 10, 220, this.handleIncluir);
+            btnEditar = new ButtonForm("Editar", 10, 260, this.handleAlterar);
+            btnExcluir = new ButtonForm("Excluir", 120, 220, this.handleExcluir);
             btnCancelar = new ButtonForm("Cancelar", 120, 260, this.handleCancel);
 
+            this.LoadInfo();
             this.Controls.Add(listView);
             this.Controls.Add(btnCadastrar);
             this.Controls.Add(btnEditar);
@@ -47,23 +50,41 @@ namespace Views
 
         }
   
-        private void CadastrarCategoria(object sender, EventArgs e)
+        public void LoadInfo() {
+            IEnumerable<Categoria> categorias = CategoriaController.GetCategoria();
+
+            this.listView.Items.Clear();
+            foreach (Categoria item in categorias)
+            {
+                ListViewItem lvItem = new ListViewItem(item.Id.ToString());
+                lvItem.SubItems.Add(item.Nome);
+                lvItem.SubItems.Add(item.Descricao);
+                
+                this.listView.Items.Add(lvItem);
+            }
+        }
+
+        private void handleIncluir(object sender, EventArgs e)
         {
             (new CadastrarCategoria()).Show();
+            this.Hide();
 
         }
-        private void EditarCategoria(object sender, EventArgs e)
+        private void handleAlterar(object sender, EventArgs e)
         {
             //(new EditarCategoria()).Show();
+            this.Hide();
 
         }
-        private void ExcluirCategoria(object sender, EventArgs e)
+        private void handleExcluir(object sender, EventArgs e)
         {
             //(new ExcluirCategoria()).Show();
+            this.Hide();
 
         }
         private void handleCancel(object sender, EventArgs e)
         {
+            this.parent.Show();
             this.Close();
         }
     }
