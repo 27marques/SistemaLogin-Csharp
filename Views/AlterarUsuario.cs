a0641702336162131450c4b5d2f2aa9629a661ee
27marques <paty_marques27@hotmail.com>
2022-05-29 19:41:33 -0300

    ViewAlterarUsuario

---
+++ Views/AlterarUsuario.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Drawing;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading;
+using Views.Lib;
+using Controllers;
+
+namespace Views
+{
+    public class AlterarUsuario : BaseForm
+    {
+        CrudUsuario parent;
+        FieldForm fieldNome;
+        FieldForm fieldEmail;
+        FieldForm fieldSenha;
+        ButtonForm btnConfirmar;
+        ButtonForm btnCancelar;
+
+        public AlterarUsuario(CrudUsuario parent) : base ("Alterar Usuário",SizeScreen.Small)
+        {
+            this.parent = parent;
+            this.parent.Hide();
+
+            fieldNome = new FieldForm("Nome",20,320,120,20);
+            fieldEmail = new FieldForm("E-mail",20,380,120,20);
+            fieldSenha = new FieldForm("Senha",20,440,100,20);
+           		
+            btnConfirmar = new ButtonForm("Confirmar", 200, 520, this.handleConfirm);
+            btnCancelar = new ButtonForm("Cancelar", 300, 520, this.handleCancel);
+
+            this.Controls.Add(fieldNome.lblField);
+            this.Controls.Add(fieldNome.txtField);
+            this.Controls.Add(fieldEmail.lblField);
+            this.Controls.Add(fieldEmail.txtField);
+            this.Controls.Add(fieldSenha.lblField);
+            this.Controls.Add(fieldSenha.txtField);
+            this.Controls.Add(btnConfirmar);
+            this.Controls.Add(btnCancelar);
+        }
+
+        private void handleConfirm(object sender, EventArgs e)
+        {
+            try {
+                UsuarioController.AlterarUsuario(
+                    this.fieldNome.txtField.Text,
+                    this.fieldEmail.txtField.Text,
+                    this.fieldSenha.txtField.Text
+                );
+                this.parent.LoadInfo();
+                this.parent.Show();
+                this.Close();
+            } catch (Exception err) {
+                MessageBox.Show(err.Message);
+            }
+
+        }
+
+        private void handleCancel(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(" Deseja mesmo sair? ", "Mensage do sistema ", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.parent.Show();
+                this.Close();
+            }
+        }
+    
+    }
+
+}
