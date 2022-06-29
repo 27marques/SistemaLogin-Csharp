38f4225af4ca1295df58f4f0066a4571d0fec9d1
27marques <paty_marques27@hotmail.com>
2022-06-29 20:13:03 -0300

    AjusteAlterarCategoriaViews29

--- Views/AlterarCategoria.cs
+++ Views/AlterarCategoria.cs
@@ -19,18 +19,28 @@ namespace Views
         FieldForm fieldDescricao;
 		ButtonForm btnConfirmar;
         ButtonForm btnCancelar;
-        
+
+        ListViewItem selectedItem;
+        int id = 0;        
 
         public AlterarCategoria(CrudCategoria parent) : base("Alterar Categoria",SizeScreen.Small)
         {
             this.parent = parent;
             this.parent.Hide();
 
+            this.selectedItem = this.parent.listView.SelectedItems[0];
+            this.id = Convert.ToInt32(this.selectedItem.Text);
+
+            Categoria categoria = CategoriaController.GetCategoria(id);
+
             fieldNome = new FieldForm("Nome",20,20,180,20);
             fieldDescricao = new FieldForm("Descrição",20,100,180,60);
 
 			btnConfirmar = new ButtonForm("Confirmar", 50, 230, this.handleConfirm);
             btnCancelar = new ButtonForm("Cancelar", 200, 230, this.handleCancel);
+
+            this.fieldNome.txtField.Text = categoria.Nome;
+            this.fieldDescricao.txtField.Text = categoria.Descricao;
 
             this.Controls.Add(fieldNome.lblField);
             this.Controls.Add(fieldNome.txtField);
